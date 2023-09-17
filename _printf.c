753cf6df8053df52d0e99576a4985c50e3bb8782
jackkuelmazier <jackkuelmazierlham@gmail.com>
2023-09-17 05:31:21 -0700

    _printf.c jack

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _printf - Print formatted text to stdout.
+ * @format: The format string.
+ * @...: The arguments to format and print.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int printed_chars = 0;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			else if (*format == 'c')
+				printed_chars += print_character(args);
+			else if (*format == 's')
+				printed_chars += print_string(args);
+			else if (*format == '%')
+				printed_chars += print_percent();
+			else if (*format == 'd' || *format == 'i')
+				printed_chars += print_integer(args);
+		}
+		else
+		{
+			_putchar(*format);
+			printed_chars++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (printed_chars);
+}
+
