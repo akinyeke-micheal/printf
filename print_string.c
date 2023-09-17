b83495049fea81d946e23b7202bdce7c8a2c67e7
akinyeke-micheal <akinyekemicheal11@gmail.com>
2023-09-17 05:47:47 -0700

    print_string added to format string to console

---
+++ print_string.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_string - Print a string.
+ *
+ * @args: variadic element
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int char_number;
+
+	if (str == NULL)
+		str = "(null)";
+
+	for (char_number = 0; str[char_number]; char_number++)
+		_putchar(str[char_number]);
+	return (char_number);
+}
