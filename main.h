0288762c4ee2e8a5a3eaad84149b8d4a61791b75
jackkuelmazier <jackkuelmazierlham@gmail.com>
2023-09-17 05:56:39 -0700

    Jack header

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+int _printf(const char *format, ...);
+int _putchar(char);
+int print_character(va_list args);
+int print_string(va_list args);
+int print_percent(void);
+int print_integer(va_list args);
+#endif
