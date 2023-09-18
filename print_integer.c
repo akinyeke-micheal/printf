c844054cc652274fafd3931a70d5ab6dd27057cf
akinyeke-micheal <akinyekemicheal11@gmail.com>
2023-09-18 02:11:13 -0700

    jack and micheal finale with print integer updated

--- print_integer.c
+++ print_integer.c
@@ -1,39 +1,42 @@
 #include "main.h"
 /**
  * print_integer - Print an integer.
- * @args: The argument list containing the integer.
- *
+ * @args: The argument list containing the integer
  * Return: The number of characters printed.
  */
 int print_integer(va_list args)
 {
-	int number;
-	int counter;
-
-	number = va_arg(args, int);
-	counter = 0;
-
-	if (number < 0)
-	{
-		_putchar('-');
-		number = -number;
-		counter++;
-	}
+	int number = va_arg(args, int);
+	int counter = 0;
+	int is_negative = 0;
+	char buffer[12];  /* Assuming INT_MIN is -2147483648 */
 
 	if (number == 0)
 	{
 		_putchar('0');
-		counter++;
+		return (1);
+	}
+
+	if (number < 0)
+	{
+		is_negative = 1;
+		number = -number;
 	}
 
 	while (number > 0)
 	{
-		int point = number % 10;
+		int digit = number % 10;
 
-		_putchar(point + '0');
+		buffer[counter++] = digit + '0';
 		number /= 10;
-		counter++;
 	}
 
-	return	(counter);
-}
+	if (is_negative)
+		buffer[counter++] = '-';
+	while (counter > 0)
+	{
+		_putchar(buffer[--counter]);
+	}
+
+	return (counter);
+i}
